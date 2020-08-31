b0aee671244c76de897efd8f458f8455610b1958
Umair <mumairafzal2010@gmail.com>
2020-08-31 16:38:29 -0400

    fix helpers

--- helpers_test.py
+++ helpers_test.py
@@ -14,41 +14,41 @@ from helpers import (
 
 class TestHelpers(unittest.TestCase):
     
-    # TESTS get_valid_payday
+    # TESTS get_valid_date
 
-    def test_get_valid_payday_positive0(self):
+    def test_get_valid_date_positive0(self):
         """Positive Test case to check if Dec 24th, 2020 is a valid payday 
            given Dec 25th, 2020 is a holiday.
         """
         holiday = date_class(2020, 12, 25)
         expected_valid_payday = date_class(2020, 12, 24)
-        valid_payday = get_valid_payday(holiday)
+        valid_payday = get_valid_date(holiday)
         assert valid_payday == expected_valid_payday
 
-    def test_get_valid_payday_positive1(self):
+    def test_get_valid_date_positive1(self):
         """Positive Test case to check if Nov 27th, 2020 is a valid payday 
            given Nov 26th, 2020 is a holiday.
         """
         holiday = date_class(2020, 11, 27)
         expected_valid_payday = date_class(2020, 11, 26)
-        valid_payday = get_valid_payday(holiday)
+        valid_payday = get_valid_date(holiday)
         assert valid_payday == expected_valid_payday
 
-    def test_get_valid_payday_negative0(self):
+    def test_get_valid_date_negative0(self):
         """Negative Test case to check if Exception is raised if 
-           invalid data is passed to 'get_valid_payday'.
+           invalid data is passed to 'get_valid_date'.
         """
         holiday = None
         expected_exception = RuntimeError
         try:
-            valid_payday = get_valid_payday(holiday)
+            valid_payday = get_valid_date(holiday)
         except Exception as exc:
             exception = exc.__class__
         assert exception == expected_exception
 
         holiday = [date_class(2020, 12, 25)]
         try:
-            valid_payday = get_valid_payday(holiday)
+            valid_payday = get_valid_date(holiday)
         except Exception as exc:
             exception = exc.__class__
         assert exception == expected_exception
